d97bdaaaff6fa49ecd8eff7bbc4dd77062852670
Kosehai <kosehai@local.com>
2022-06-24 15:15:22 +0200

    started some gui testing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-use anyhow::Result;
-use obws::Client;
-use tokio::time::{sleep, Duration};
-use std::env;
+fn main() {
+    MainWindow::new().run();
+}
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let ip = "10.240.236.111";
-    let port = 4444;
-    let pass = env::var("OBSPASS").unwrap().to_string();
-    let client = Client::connect(ip, port).await?;
-    client.login(Some(pass)).await?;
-    //test
-    client.recording().start_recording().await?;
-    sleep(Duration::from_millis(5000)).await;
-    client.recording().stop_recording().await?;
-    Ok(())
+slint::slint! {
+    MainWindow := Window {
+        Text {
+            text: "hello world";
+            color: green;
+        }
+    }
 }
